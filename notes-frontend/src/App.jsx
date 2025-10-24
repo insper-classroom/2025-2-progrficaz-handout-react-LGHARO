b6c76e32731510cca9c18d745fdc0376ea28b53e
Lucas Haro <lgharo06@gmail.com>
2025-10-24 04:43:33 -0300

    algo

--- notes-frontend/src/App.jsx
+++ notes-frontend/src/App.jsx
@@ -1,10 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Note from "./components/Note";
 import "./App.css";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/notes/")
+      .then((res) => setNotes(res.data));
+  }, []);
+
+  console.log(notes);
+
   return (
-    <Note />
+    <>
+      {notes.map((note) => (
+        <Note key={`note__${note.id}`} title={note.title}>
+          {note.content}
+        </Note>
+      ))}
+    </>
   );
 }
-	
-export default App;	
+
+export default App;
